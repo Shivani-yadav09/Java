43c0feadf853257caa4dfe977f429f7bec87ca91
Avula Shivani Yadav <149299336+Shivani-yadav09@users.noreply.github.com>
2026-08-24 22:09:59 +0530

    Add files via upload

---
+++ Patterns/MissingNumber.java
@@ -0,0 +1,20 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 5, 6};
+
+        int n = 6;
+
+        int total = n * (n + 1) / 2;
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        int missing = total - sum;
+
+        System.out.println("Missing number = " + missing);
+    }
+}
