e6cb1a60b8184996de0f4c521cd96a74fe14b527
Avula Shivani Yadav <149299336+Shivani-yadav09@users.noreply.github.com>
2026-08-27 21:50:07 +0530

    Add files via upload

---
+++ Patterns/FirstRepeated.java
@@ -0,0 +1,19 @@
+public class FirstRepeated {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 20, 40, 10};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println("First repeated element = " + arr[i]);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No repeated element");
+    }
+}
